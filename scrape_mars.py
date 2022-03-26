4bc9fdaa86d98f53d570e90b98e0c0bebb4790b0
Marquetta <73968023+quettamar@users.noreply.github.com>
2022-03-25 23:46:31 -0500

    set up scrape_mars.py and app.py file

--- scrape_mars.py
+++ scrape_mars.py
@@ -9,41 +9,79 @@ def scrape_info():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    url = "https://redplanetscience.com/"
     browser.visit(url)
 
-    time.sleep(1)
-
-    # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Get the average temps
-    title = soup.find('div', class_="content-title")
+    #use soup to find images/info
+    news_title = soup.find('div', class_="content_title").text
+    news_p = soup.find('div', class_="article_teaser_body").text
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
-
-
-    url = "https://visitcostarica.herokuapp.com/"
+    url = "https://spaceimages-mars.com"
     browser.visit(url)
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
+
+    #this cell is automating clicking the full image button
+    full_image_btn = browser.find_by_tag('button')[1]
+    full_image_btn.click()
+
+    root_url = "https://spaceimages-mars.com/"
+    html = browser.html
+    soup = bs(html, "html.parser")
+    #if the item is embedded or I have to click a button, be sure to be looking at what I need to search for before running the soup or have that click function 
+    #in the cell above
+    image = soup.find('img', class_="fancybox-image")
+    image_url = image.get('src')
+    featured_img_url = root_url + image_url
+
+    url = "https://galaxyfacts-mars.com"
+    browser.visit(url)
+
+    # I found table first using beatifulsoup
+    # mars_table = soup.find('table', class_="table table-striped")
+    #use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
+    df = pd.read_html("https://galaxyfacts-mars.com")[0]
+    df.columns=["Description", "Mars", "Earth"]
+    df.set_index("Description", inplace=True)
+
+    url = "https://marshemispheres.com/"
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    results = soup.find_all('div',class_='item')
+    hemisphere_image_urls = []
+    for result in results:
+        image_dict = {}
+        title = result.find("h3").text
+        link = result.find("a")["href"]
+        image_link = "https://marshemispheres.com/" + link
+        browser.visit(image_link)
+        html = browser.html
+        soup= bs(html, "html.parser")
+        full_size_img = soup.find("img", class_="wide-image")['src']
+        full_size_img_link = "https://marshemispheres.com/" + full_size_img
+        print(title)
+        print(image_link)
+        print(full_size_img_link)
+        image_dict['title']= title
+        image_dict['image_url']= full_size_img_link
+        hemisphere_image_urls.append(image_dict)
+
+    mars_data = {
+    "news title" : news_title,
+    "news_p" : news_p,
+    "featured_img_url" : featured_img_url,
+    "df" : df,
+    "hemisphere_image_urls" : hemisphere_image_urls
     }
 
-    # Close the browser after scraping
-    browser.quit()
+    #Return results
+    return mars_data
 
-    # Return results
-    return costa_data
+#Close the browser after scraping
+browser.quit()
+
+
